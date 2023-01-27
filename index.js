c148ceb838588f1cb415f5699b19a6bc200bb4ea
Usman AR <usman8529@gmail.com>
2023-01-27 16:50:47 +0530

    Added functionalities

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const ejs = require("ejs")
 const express= require('express')
 const mong = require("mongoose");
 const port =  process.env.PORT || 5000;
-
+const bodyParser = require('body-parser');
 const app=express()
 app.use( express.static( "public" ) );
 app.set('view engine','ejs');
-mong.connect("mongodb://localhost:27017/Users")
+mong.connect("mongodb://localhost:27017/Portfolio_db")
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+
 // Admin/User Schema 
 const schema = new mong.Schema({
     first_name:String,
@@ -20,13 +27,106 @@ const schema = new mong.Schema({
     updated_time:Date
 })
 
+const user = new mong.model("user",schema)
+
 app.get('/login',(req,res)=>{
-    res.render('main')
+    res.render('login')
 })
 
 app.get('/add',(req,res)=>{
-    
-    res.render('add')
+
+    res.render('add',{
+        user:true
+    })
+})
+
+app.get('/update',(req,res)=>{
+    user.find({}).then((use)=>{
+        res.render('update',{
+            users:use
+        })
+    })
+  // res.render('update')
+   
+})
+
+app.get('/view',(req,res)=>{
+
+    user.find({}).then((users)=>{
+        res.send(users);
+    }).catch((error)=>{
+        res.status(500).send(error)
+    })
+//    res.render('update')
+})
+
+
+app.get('/edit/:name',(req,res)=>{
+    user.findOne({first_name:req.params.name}).then((response)=>{
+        res.render('edit',{
+            user:response
+        })
+    })
+})
+
+app.post('/edit/:name',(req,res)=>{
+    user.updateOne({first_name:req.params.name},{
+        first_name:req.body.fname,
+        middle_name:req.body.mname,
+        last_name:req.body.lname,
+        email:req.body.email,
+        role:req.body.role,
+        department:req.body.dept,
+    }).then((response)=>{
+        user.find({}).then((use)=>{
+            res.render('update',{
+                users:use
+            })
+        })
+    })
+})
+
+// app.get('/view/:field',(req,res)=>{
+//     const fields={
+//         1:"first_name",
+//         2:"middle_name",
+//         3:"last_name",
+//         4:"email",
+//         6:"role",
+//         7:"department"
+//     }
+
+//     user.find({}).then((users)=>{
+//         if(req.params.field==5){
+//             res.send("Cannot display password")
+//         }
+//         else if(req.params.field>=1&&req.params.field<=7)
+//         res.send(fields[req.params.field] + " : " + users[0][fields[req.params.field]]);
+//         else res.send("Enter proper field number")
+//     }).catch((error)=>{
+//         res.status(500).send(error)
+//     })
+// //    res.render('update')
+// })
+
+
+
+
+app.post('/add',(req,res)=>{
+    console.log("The name taken is"  + req.body.fname)
+    const new_user = new user({
+        first_name:req.body.fname,
+        middle_name:req.body.mname,
+        last_name:req.body.lname,
+        email:req.body.email,
+        role:req.body.role,
+        department:req.body.dept,
+
+    })
+    new_user.save();
+    res.render('add',{
+        user:true
+    })
 })
 
 
